375fb3323bdb69d94ad4999e075d884ebabe11ed
anouar <medanouar.grami@gmail.com>
2021-03-26 11:53:23 +0100

    JSX check

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import './Style.css'
+import anouar from './Anonymous-child-748192.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{border:'solid 1px black', maxWidth:'100vw'}}>
+
+<h1 classname="title red">anouar</h1>
+
+<br />
+
+<img src={anouar} />
+
+<br/>
+
+<img src="/2IxPQ1iF_400x400.jpg" />
+
+</div>
+
+<video width="320" height="240" controls>
+
+<source src="myVideo.mp4" type="video/mp4" />
+
+</video>
     </div>
   );
 }
 
-export default App;
+export default App ;
